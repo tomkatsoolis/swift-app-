90bde4ad848d0714a7d0dabb94087a84e72cd42b
tom katsoolis <tomkatsoolis@toms-macbook-pro.local>
2017-03-17 12:40:30 +0800

    Initial Commit

---
+++ R Take 5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  R Take 5
+//
+//  Created by tom katsoolis on 17/3/17.
+//  Copyright © 2017 Raytheon Australia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
