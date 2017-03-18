c7d87ea38b69d8e1412f0ba0b67c552cee542715
tom katsoolis <thomas.katsoolis@gmail.com>
2017-03-18 13:17:28 +0800

    my first commit

--- R Take 5/ViewController.swift
+++ R Take 5/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var helloworld: UILabel!
+// tap count function used to count number of taps and added a helloworld.text output
+    var tapcount = 0
+    
+    @IBAction func buttontapped(_ sender: Any) {
+         self.helloworld.text = "welcome to take 5"
+        print("button tapped")
+            tapcount = tapcount + 1
+            print (tapcount)
+        if tapcount >= 10 { helloworld.text = "you tapped the button 10 times"
+        }
+    }
+// second button 
+    @IBAction func button2(_ sender: Any) {
+        
+        self.helloworld.text = "you pressed the second button"
+        print("button tapped")  }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+// Do any additional setup after loading the view, typically from a nib.
+       
+
+        self.view.backgroundColor = UIColor.red
+        
     }
 
     override func didReceiveMemoryWarning() {
